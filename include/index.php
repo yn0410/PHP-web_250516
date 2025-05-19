e5d57bdc73ba09ddb5bd397d3d233ba2b085a49a
yn <jenny783113@gmail.com>
2025-05-19 14:05:21 +0800

    PHP L3 實作

--- include/index.php
+++ include/index.php
@@ -7,20 +7,8 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <?php 
-    for($i=0; $i<5; $i++){
-        // include "header.php";
-        include_once "header.php";
-    }
-    // require "header.php";
-    ?>
-    <main>
-        <h2>歡迎來到學生管理系統</h2>
-        <p>請從上方選單選擇功能。</p>
-    </main>
-    <?php 
-    include "footer.php";
-    // require "footer.php";
-    ?>
+    <?php include_once "header.php";?>
+    <?php include "main.php";?>
+    <?php include "footer.php";?>
 </body>
 </html>
