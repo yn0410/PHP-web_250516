3728f6ee19ef621b182fd64ebe4bdac6c0663bb5
yn <jenny783113@gmail.com>
2025-05-19 15:27:17 +0800

    PHP L3 include應用

--- include/index.php
+++ include/index.php
@@ -1,15 +1,38 @@
+<?php 
+    $page=isset($_GET['page'])?$_GET['page']:'main';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>學生管理系統</title>
+    <title>學生管理系統
+    <?php
+        switch($page){
+            case 'list':
+                echo " - 學生列表";
+                break;
+            case 'new':
+                echo " - 新增學生";
+                break;
+            case 'query':
+                echo " - 查詢學生";
+                break;
+            case 'about':
+                echo " - 關於我們";
+                break;
+            default:
+                echo " - 首頁";
+
+        }
+    ?>
+    </title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <?php include_once "header.php";?>
     <?php 
-    $page=isset($_GET['page'])?$_GET['page']:'main';
+    // $page=isset($_GET['page'])?$_GET['page']:'main';
     // $page = $_GET['page'] ?? 'main'; //上一行的更精簡
 
     $file = $page . ".php";
