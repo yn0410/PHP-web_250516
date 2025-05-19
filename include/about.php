8af5aadba840314ec70e53be4715300a58c60a2b
yn <jenny783113@gmail.com>
2025-05-19 13:30:04 +0800

    PHP L3 include實作-header &新增功能

--- include/about.php
+++ include/about.php
@@ -50,17 +50,7 @@
     </style>
 </head>
 <body>
-    <header>
-        <h1>學生管理系統</h1>
-    </header>
-    <nav>
-        <ul>
-            <li><a href="index.php">首頁</a></li>
-            <li><a href="list.php">學生列表</a></li>
-            <li><a href="new.php">新增學生</a></li>
-            <li><a href="about.php">關於</a></li>
-        </ul>
-    </nav>
+    <?php include "header.php"; ?>
     <main>
         <h2>歡迎來到學生管理系統</h2>
         <p>關於我們</p>
--- include/query.php
+++ include/query.php
@@ -53,7 +53,7 @@
     <?php include "header.php"; ?>
     <main>
         <h2>歡迎來到學生管理系統</h2>
-        <p>新增學生</p>
+        <p>查詢學生</p>
     </main>
         <?php include "footer.php"; ?>
 </body>
